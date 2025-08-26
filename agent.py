e3fbeb13f85a9fcc623fd96c8004eb92e658cad4
xlx98005 <xlx98005@gmail.com>
2025-08-26 18:52:32 +0800

    refactor: remove unused run.py script and related functions

--- agent.py
+++ agent.py
@@ -84,33 +84,3 @@ def roast_agent(content: str, model: ChatOpenAI, tools: list[tool]) -> dict:
     messages = [SystemMessage(content=roast_prompt), HumanMessage(content=content)]
     resp = agent.invoke({"messages": messages})
     return resp
-
-
-# def review_agent(query: str, model: ChatOpenAI, tools: list[tool]):
-#     model_with_tools = model.bind_tools(tools)
-#     model_output = model_with_tools.invoke(query)
-#     tool_resp = call_tool(model_output)
-#     final_resp = model.invoke(
-#         f'original query : {query} \n\n\n tool response : {tool_resp}')
-#     return final_resp
-
-# def call_tool(model_output, tools):
-#     tools_map = {tool.name.lower(): tool for tool in tools}
-#     tools_resp = {}
-#     for tool in model_output.tool_calls:
-#         tool_name = tool['name']
-#         tool_args = tool['args']
-#         tool_instance = tools_map[tool_name]
-#         tool_resp = tool_instance.invoke(*tool_args.values())
-#         tools_resp[tool_name] = tool_resp
-#     return tools_resp
-
-# def output_parser(output: str):
-#     parser_model =ChatOpenAI(
-#         model = 'glm-3-turbo',
-#         temperature=0.2,
-#         openai_api_base = "https://open.bigmodel.cn/api/paas/v4",
-#         api_key=os.getenv("OPENAI_API_KEY"),
-#     )
-#     message = f"你需要将传入的文本改写，尽可能自然。 这是你需要改写的文本：`{text}`"
-#     return parser_model.invoke(message.format(text=output))
